59e906f0f6942a661a1cbc1ce0fc47a5cc5cbbbb
orlein <pcj9024@gmail.com>
2024-09-11 20:23:28 +0900

    add button

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,12 @@ import type { Metadata } from "next";
 import { Counter } from "./components/counter/Counter";
 
 export default function IndexPage() {
-  return <Counter />;
+  return (
+    <div>
+      <Counter />
+      <button>Button Click!</button>
+    </div>
+  );
 }
 
 export const metadata: Metadata = {
